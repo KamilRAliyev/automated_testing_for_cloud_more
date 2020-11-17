37134f41012b44625bd295ec89e0fc4ce15e80ee
Kamil Aliyev <kamiraliyev@gmail.com>
2020-11-17 21:15:40 +0400

    ScreenshotTesting.py done

--- ScreenshotTesting.py
+++ ScreenshotTesting.py
@@ -1,70 +1,90 @@
 import unittest
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import os.path
 
 WEBSITE_URL = "https://web.cloudmore.com/"
 
-class DesktopScreenshotTesting(unittest.TestCase):
+class DesktopSizeScreenshotTesting(unittest.TestCase):
     @classmethod
     def setUpClass(inst):
         # create a new Firefox session
         inst.driver = webdriver.Firefox(executable_path="./webdriver/geckodriver")
         inst.driver.implicitly_wait(30)
-        inst.driver.minimize_window()
+        inst.driver.maximize_window()
         # navigate to the application home page
         inst.driver.get(WEBSITE_URL)
         inst.driver.title
 
-    def test_screenshot(self):
+    def consent_banner(self):
+        if self.driver.find_element_by_css_selector("a#hs-eu-confirmation-button"):
+            self.driver.find_element_by_css_selector("a#hs-eu-confirmation-button").click()
+            self.driver.implicitly_wait(30)
+
+    def test_search_hogset(self):
         search_key = "Högset"
+        self.consent_banner()
         self.driver.find_element_by_css_selector('span.search > i').click()
         search_box = self.driver.find_element_by_css_selector("input[type='search'].hs-input")
         search_box.send_keys(search_key)
         search_button = self.driver.find_element_by_css_selector("button[type='submit'].hs-button")
         search_button.click()
-        self.driver.implicitly_wait(30)
-        iterator = 0
-        while(self.driver.find_element_by_css_selector("a.hs-search-results__next-page") and iterator <=3):
-            self.driver.find_element_by_css_selector("a.hs-search-results__next-page").click()
-        self.driver.get_screenshot_as_file("screenshot.png")
-        self.assertTrue(os.path.isfile('screenshot.png'), msg="Desktop size image is not found.")
+        self.driver.implicitly_wait(60)
+        for i in range(2):
+            try:
+                self.driver.find_element_by_css_selector("a.hs-search-results__next-page").click()
+                self.driver.implicitly_wait(30)
+            except:
+                break
+        self.driver.get_screenshot_as_file("screenshot_desktop.png")
+        self.assertTrue(os.path.isfile('screenshot_desktop.png'), msg="Desktop size image is not found.")
+    
+    
 
     @classmethod
     def tearDownClass(inst):
         # close the browser window
         inst.driver.quit()
 
-class MobileScreenshotTesting(unittest.TestCase):
+class MobileSizeScreenshotTesting(unittest.TestCase):
     @classmethod
     def setUpClass(inst):
         # create a new Firefox session
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--window-size=360,640')
-        inst.driver = webdriver.Firefox(executable_path="./webdriver/geckodriver", options=options)
+        inst.driver = webdriver.Firefox(executable_path="./webdriver/geckodriver")
         inst.driver.implicitly_wait(30)
-        inst.driver.minimize_window()
+        inst.driver.set_window_size(375,812)
         # navigate to the application home page
         inst.driver.get(WEBSITE_URL)
         inst.driver.title
 
-    def test_screenshot(self):
+    def consent_banner(self):
+        if self.driver.find_element_by_css_selector("a#hs-eu-confirmation-button"):
+            self.driver.find_element_by_css_selector("a#hs-eu-confirmation-button").click()
+            self.driver.implicitly_wait(30)
+
+    def test_search_hogset(self):
         search_key = "Högset"
+        self.consent_banner()
         self.driver.find_element_by_css_selector('span.search > i').click()
         search_box = self.driver.find_element_by_css_selector("input[type='search'].hs-input")
         search_box.send_keys(search_key)
         search_button = self.driver.find_element_by_css_selector("button[type='submit'].hs-button")
         search_button.click()
-        self.driver.implicitly_wait(30)
-        iterator = 0
-        while(self.driver.find_element_by_css_selector("a.hs-search-results__next-page") and iterator <=3):
-            self.driver.find_element_by_css_selector("a.hs-search-results__next-page").click()
+        self.driver.implicitly_wait(60)
+        for i in range(2):
+            try:
+                self.driver.find_element_by_css_selector("a.hs-search-results__next-page").click()
+                self.driver.implicitly_wait(30)
+            except:
+                break
         self.driver.get_screenshot_as_file("screenshot_mobile.png")
-        self.assertTrue(os.path.isfile('screenshot_mobile.png'), msg="Mobile size image is not found.")
+        self.assertTrue(os.path.isfile('screenshot_mobile.png'), msg="Desktop size image is not found.")
+    
+    
 
     @classmethod
     def tearDownClass(inst):
         # close the browser window
         inst.driver.quit()
+if __name__ == '__main__':
+    unittest.main()
