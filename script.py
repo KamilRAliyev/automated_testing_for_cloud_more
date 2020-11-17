2e58b320d26d7acc49c3ae6f7cf828e960f112f1
Kamil Aliyev <kamiraliyev@gmail.com>
2020-11-18 03:36:54 +0400

    Finito

--- script.py
+++ script.py
@@ -24,11 +24,41 @@ ScreenshotMobileTest = TestLoader().loadTestsFromTestCase(tests.ScreenshotTestin
 SearchResultsTest = TestLoader().loadTestsFromTestCase(tests.SearchTesting.SearchResultsTesting)
 SearchNoResultsTest = TestLoader().loadTestsFromTestCase(tests.SearchTesting.SearchNoResultTesting)
 
-suite = TestSuite([ElementTest, MenuAboutUsTest, MenuBlogTest, MenuCaseStudiesTest, MenuContactUsTest, MenuPlatformTest, MenuSolutionTest, ScreenshotDesktopTest, ScreenshotMobileTest, SearchResultsTest, SearchResultsTest])
+suite1 = TestSuite([ElementTest,])
+suite2 = TestSuite([MenuAboutUsTest,])
+suite3 = TestSuite([MenuBlogTest,])
+suite4 = TestSuite([MenuCaseStudiesTest,])
+suite5 = TestSuite([MenuContactUsTest,])
+suite6 = TestSuite([MenuPlatformTest,])
+suite7 = TestSuite([MenuSolutionTest,])
+suite8 = TestSuite([SearchResultsTest,])
+suite9 = TestSuite([SearchNoResultsTest,])
+suite10 = TestSuite([ScreenshotMobileTest,])
+suite11 = TestSuite([ScreenshotDesktopTest,])
+
 kwargs = {
-    "output": "reports",
-    "report_name": "testing_result",
+    "output": ".",
     "failfast": True
 }
-runner = HTMLTestRunner(**kwargs)
-runner.run(suite)
+runner = HTMLTestRunner(report_name="ElementTest",**kwargs)
+runner.run(suite1)
+runner = HTMLTestRunner(report_name="MenuAboutUsTest",**kwargs)
+runner.run(suite2)
+runner = HTMLTestRunner(report_name="MenuBlogTest",**kwargs)
+runner.run(suite3)
+runner = HTMLTestRunner(report_name="MenuCaseStudiesTest",**kwargs)
+runner.run(suite4)
+runner = HTMLTestRunner(report_name="MenuContactUsTest",**kwargs)
+runner.run(suite5)
+runner = HTMLTestRunner(report_name="MenuPlatformTest",**kwargs)
+runner.run(suite6)
+runner = HTMLTestRunner(report_name="MenuSolutionTest",**kwargs)
+runner.run(suite7)
+runner = HTMLTestRunner(report_name="SearchResultsTest",**kwargs)
+runner.run(suite8)
+runner = HTMLTestRunner(report_name="SearchNoResultsTest",**kwargs)
+runner.run(suite9)
+runner = HTMLTestRunner(report_name="ScreenshotMobileTest",**kwargs)
+runner.run(suite10)
+runner = HTMLTestRunner(report_name="ScreenshotDesktopTest",**kwargs)
+runner.run(suite11)
